ec385b14fc4e511d78d27e4932ae5ac0ae279f3b
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-06 17:02:45 +0200

    importazioni

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <HelloWorld msg="Prova"/>
   </div>
 </template>
 
@@ -17,12 +17,13 @@ export default {
 </script>
 
 <style lang="scss">
+@import "~bootstrap/scss/bootstrap";
+@import "~@fontsource/montserrat/400.css";
+@import "~@fontsource/montserrat/600.css";
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
+  
 }
 </style>
